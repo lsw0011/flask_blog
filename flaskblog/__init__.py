e7aa4696f77c94bbc0505f610d57b9a4dc2c2c25
Luke Westerfield <lukeswesterfield@gmail.com>
2018-09-27 17:55:22 -0400

    added login functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-		
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '94fa092f521fc561151c4f5c313c0f6f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes

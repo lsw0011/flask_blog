bfa0a2b52bb8a4cf029857526ee5272db769a825
Luke Westerfield <lukeswesterfield@gmail.com>
2018-09-30 22:42:19 -0400

    blueprints

---
+++ flaskblog/config.py
@@ -0,0 +1,8 @@
+class Config:
+	SECRET_KEY = '94fa092f521fc561151c4f5c313c0f6f'
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = '################@gmail.com'
+	MAIL_PASSWORD = '###################################'
